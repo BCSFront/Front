e97bcf9afe27a31fa58e490621c173a88c75b628
g9h22 <g9h22@naver.com>
2023-09-02 19:51:02 +0900

    Update modal

--- src/components/GModal.jsx
+++ src/components/GModal.jsx
@@ -4,49 +4,43 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 
 const GModal = ({ title, title2 }) => {
-    const { num, setNum, showModal, setShowModal } = useContext(AppContext);
-    const onClickBtn = () => {
-        setNum(num + 100);
-        setShowModal(!showModal);
-    };
-    return (
-        <div className=" mx-auto">
-            <div className="gmodal-box1  m-[20px]">
-                <div>
-                    <div className="flex mb-[24px]">
-                        <img
-                            className="mr-[16px]"
-                            src="../images/Group.svg"
-                            alt="보내기 "
-                        />
-                        {/* <div className="mr-[16px] w-[226px] gmodal-text1">
+  const { num, setNum, showModal, setShowModal } = useContext(AppContext);
+  const onClickBtn = () => {
+    setNum(num + 100);
+    setShowModal(!showModal);
+  };
+  return (
+    <div className=" mx-auto ">
+      <div className="gmodal-box1  m-[20px]">
+        <div>
+          <div className="flex mb-[24px]">
+            <img
+              className="mr-[16px]"
+              src="../images/Group.svg"
+              alt="보내기 "
+            />
+            {/* <div className="mr-[16px] w-[226px] gmodal-text1">
                             Correct!
                         </div> */}
-                        <div className="mr-[16px] w-[226px] gmodal-text1">
-                            {title}
-                        </div>
-                        <img
-                            className="mr-[20px] "
-                            src="../images/MaskGroup.svg"
-                            alt="보내기 "
-                        />
-                        <img
-                            className=" "
-                            src="../images/gmodal-i.svg"
-                            alt="보내기 "
-                        />
-                    </div>
-                </div>
-                <Link to="/detail/2">
-                    <button
-                        className="gmodal-button gmodal-button-text"
-                        onClick={onClickBtn}
-                    >
-                        {title2}
-                    </button>
-                </Link>
-            </div>
+            <div className="mr-[16px] w-[226px] gmodal-text1">{title}</div>
+            <img
+              className="mr-[20px] "
+              src="../images/MaskGroup.svg"
+              alt="보내기 "
+            />
+            <img className=" " src="../images/gmodal-i.svg" alt="보내기 " />
+          </div>
         </div>
-    );
+        <Link to="/detail/2">
+          <button
+            className="gmodal-button gmodal-button-text"
+            onClick={onClickBtn}
+          >
+            {title2}
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default GModal;
--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -11,51 +11,72 @@ import { AppContext } from "../App";
 import DetailQuestion from "../components/DetailQuestion";
 
 const Detail = () => {
-    const { id } = useParams();
-    const [answer, setAnswer] = useState();
-    const { num, setNum, showModal, setShowModal } = useContext(AppContext);
+  const { id } = useParams();
+  const [answer, setAnswer] = useState();
+  const { num, setNum, showModal, setShowModal } = useContext(AppContext);
 
-    useEffect(() => {
-        console.log(answer);
-    }, [answer]);
+  useEffect(() => {
+    console.log(answer);
+  }, [answer]);
 
-    return (
-        <div className="overflow-y-auto">
-            <div className="flex flex-col justify-center min-h-screen">
-                <Link to="/topic">
-                    <Arrow2 num={num} />
-                </Link>
+  return (
+    <>
+      <div className="overflow-y-auto flex">
+        <div className="flex flex-col justify-center min-h-screen">
+          <Link to="/topic">
+            <Arrow2 num={num} />
+          </Link>
 
-                <DetailQuestion
-                    text={data.question[id - 1].text}
-                    img={data.question[id - 1].img}
+          <DetailQuestion
+            text={data.question[id - 1].text}
+            img={data.question[id - 1].img}
+          />
+
+          <div className="line mt-[33px] mb-[20px]"></div>
+
+          <div className="mt-[24px] mx-[39px] mb-[52px]">
+            <div>
+              {data.detail[id - 1].map((v, i) => (
+                <Detail1Box
+                  key={i}
+                  detail={v}
+                  answer={answer}
+                  setAnswer={setAnswer}
                 />
-
-                <div className="line mt-[33px] mb-[20px]"></div>
-
-                <div className="mt-[24px] mx-[39px] mb-[52px]">
-                    <div>
-                        {data.detail[id - 1].map((v, i) => (
-                            <Detail1Box
-                                key={i}
-                                detail={v}
-                                answer={answer}
-                                setAnswer={setAnswer}
-                            />
-                        ))}
-                    </div>
-                </div>
-
-                <div
-                    className=" checkABox ml-[21px] "
-                    onClick={() => setShowModal(!showModal)}
-                >
-                    <BtnContinue title="CHECK ANSWER" />
-                </div>
-
-                {showModal && <GModal title="Correct!" title2="CONTINUE" />}
+              ))}
             </div>
+          </div>
+
+          <div
+            className=" checkABox ml-[21px] "
+            onClick={() => setShowModal(!showModal)}
+          >
+            <BtnContinue title="CHECK ANSWER" />
+          </div>
         </div>
-    );
+      </div>
+      <div className="  ">
+        {showModal && (
+          <div
+            class="relative z-10"
+            aria-labelledby="modal-title"
+            role="dialog"
+            aria-modal="true"
+          >
+            <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+            <div class="fixed inset-0 z-10 overflow-y-auto">
+              <div class="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
+                <GModal
+                  title="Correct!"
+                  title2="CONTINUE"
+                  className="relative transform overflow-hidden rounded-lg text-left shadow-xl transition-all "
+                />
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 export default Detail;
